37d6c0057bc3ececfa555ff2584218ea22eea643
Mark Wagner <mwagner72@gmail.com>
2013-04-03 18:39:37 -0500

    fixed paramRoot usage in backbone_rails_sync

--- vendor/assets/javascripts/backbone_rails_sync.js
+++ vendor/assets/javascripts/backbone_rails_sync.js
@@ -14,6 +14,22 @@
         }
       }, options);
     }
+
+
+    // Serialize data, optionally using paramRoot
+    if (options.data == null && model && (method === 'create' || method === 'update' || method === 'patch')) {
+      options.contentType = 'application/json';
+      data = JSON.stringify(options.attrs || model.toJSON(options));
+
+      if (model.paramRoot) {
+        data = {};
+        data[model.paramRoot] = model.toJSON(options);
+      } else {
+        data = model.toJSON();
+      }
+      options.data = JSON.stringify(data);
+    }
+
     Backbone.original_sync(method, model, options)
 
   }
